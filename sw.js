d65797ff516d9b1529d2a9da843c0d9cf8afa1fa
libertidesigns <liberti.designs@gmail.com>
2026-03-18 01:52:35 +0100

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE='liberti-fit-v1';
-const ASSETS=['./','/index.html','/manifest.json'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
-self.addEventListener('fetch',e=>{if(e.request.url.includes('script.google.com')||e.request.url.includes('googleapis'))return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{if(!res||res.status!==200||res.type!=='basic')return res;const rc=res.clone();caches.open(CACHE).then(c=>c.put(e.request,rc));return res;})));});
